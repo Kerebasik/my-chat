43e0b651b121aaeede447e1859737ecb4d7c88d5
kerebasik <kirill.denisenko.01@gmail.com>
2025-05-03 13:20:31 +0300

    Feat: AuthModule with JWT authorization and user sign up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {Body, Controller, HttpStatus, HttpCode, Post} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import {CreateUserDto} from "../users/dto/create-user.dto";
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Get('login')
-  login() {
-    return this.authService.login();
+  @Post('log-in')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() userDto: CreateUserDto) {
+   return this.authService.login(userDto);
   }
 
-  @Post('signup')
-  signup() {
-    return this.authService.signup();
+  @Post('sign-up')
+  async signup(@Body() userDto:CreateUserDto) {
+    await this.authService.signup(userDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,32 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../modules/prisma/prisma.service';
-import { User, Prisma } from '../../generated/prisma/client';
-import { CreateUserDto } from './dto/create-user.dto';
+import {Injectable} from '@nestjs/common';
+import {PrismaService} from '../modules/prisma/prisma.service';
+import {User, Prisma} from '../../generated/prisma/client';
+import {CreateUserDto} from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) {
+    }
 
-  async users(params: {
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.UserWhereUniqueInput;
-    where?: Prisma.UserWhereInput;
-    orderBy?: Prisma.UserOrderByWithRelationInput;
-  }): Promise<User[]> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return this.prisma.user.findMany({
-      skip,
-      take,
-      cursor,
-      where,
-      orderBy,
-    });
-  }
+    async user(params:Prisma.UserWhereUniqueInput): Promise<User | null> {
+        const user = await this.prisma.user.findUnique({
+            where: params
+        });
+        return user;
+    }
 
-  async create(dto: CreateUserDto): Promise<User> {
-    return this.prisma.user.create({
-      data: dto,
-    });
-  }
+    async users(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.UserWhereUniqueInput;
+        where?: Prisma.UserWhereInput;
+        orderBy?: Prisma.UserOrderByWithRelationInput;
+    }): Promise<User[]> {
+        const {skip, take, cursor, where, orderBy} = params;
+        return this.prisma.user.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+        });
+    }
+
+    async create(data:CreateUserDto): Promise<User> {
+        return this.prisma.user.create({
+            data:data
+        });
+    }
 }
